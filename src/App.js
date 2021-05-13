bbf0712c5cc81dc80246b93b797e7f49c443d964
Jacob moya <jemoya1994@gmail.com>
2021-05-13 14:44:40 -0700

    first commit, first section infrastructure added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// mainlogo for header
+import Logo from './images/logo.png';
+import './styles/app.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='welcome_section'>
+        <nav>
+          <img src={Logo} alt="jmoya_logo"/>
+          <div>
+            <i className="fa fa-bars"></i>
+          </div>
+        </nav>
+        <section className='welcome_text'>
+          <h1>
+            <span className='bigtext'>Hey</span>, I am <span className='keyword changing_text'>Jacob Moya</span>!
+          </h1>
+          <h1 className='welcome_description'>
+            I am a <span className='keyword changing_text'>Front-End Developer</span>, with a keen eye for design! 
+          </h1>
+        </section>
+        <section className='welcome_intro'>
+          <div className='greeting'>
+            <h1>
+              Nice to meet you.
+            </h1>
+          </div>
+          <div className='message'>
+            <p>
+              I am a passionate Front-End developer! 
+              From a wireframe all the way to launch, 
+              I diligintly build beautiful sites with 
+              a variety of different toolsets... Read
+              more about the tech I use <span className='keyword changing_text'>below</span>. 
+            </p>
+          </div>
+        </section>
+
+      </section>
+
     </div>
   );
 }
