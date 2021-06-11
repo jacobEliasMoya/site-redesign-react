174baf0715e96b7a786fd8c7f1ae3d5d8b763ae8
jacob moya <jemoya1994@Gmail.com>
2021-06-10 22:23:19 -0700

    added beautiful nav bar

--- src/MainFooter.js
+++ src/MainFooter.js
@@ -2,13 +2,20 @@ import React from 'react';
 import NavmenuUL from './NavmenuUL';
 import QuickContact from './QuickContact';
 import Spacer from './Spacer';
+import Logo from './images/logo.png';
 
 export default function MainFooter() {
     return (
         <footer>
             <QuickContact />
             <NavmenuUL />
-            <Spacer />
+            <div>
+                <span>
+                    <a href="welcome_section"><img src={Logo} /></a>
+                    </span>
+                <p> &copy; Jacobmoya.com 2021 </p>
+            </div>
+
         </footer>
     )
 }
